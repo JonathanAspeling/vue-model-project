27ed6b0a732139e4b834a5e8be4cab4e346304c1
Jonathan Aspeling <jonathan@amberstone.digital>
2021-04-20 21:56:23 +0200

    Href portion for Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
 <h1>{{title}}</h1>
+<input type="text" ref="name">
+<button @click="handleClick">Click me</button>
 </template>
 
 <script>
@@ -10,6 +12,14 @@ export default {
     return{
       title:'My first Vue App :)',
     }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+
   }
 };
 </script>
