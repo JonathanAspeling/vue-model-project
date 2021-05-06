fd9a9da9ceef009c0ded9c69a7ea22986d5595db
Jonathan Aspeling <jonathan@amberstone.digital>
2021-05-06 07:41:16 +0200

    CSS Scope and tricks with Vue part completed

--- src/components/Model.vue
+++ src/components/Model.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="backdrop">
       <div class="model">
+          <h1>Model Title</h1>
           <p>Model Content</p>
       </div>
   </div>
@@ -25,6 +26,14 @@ export default {};
         background: rgba(0,0,0,0.5);
         width: 100%;
         height: 100%;
+    }
+    .model h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    .model p{
+        font-style: normal;
     }
 
 </style>
